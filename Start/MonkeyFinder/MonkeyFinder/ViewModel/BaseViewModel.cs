8ba92f29bb7c910e8bc7a4b30213e745d1d5c083
Aaron Wood <aaronwood5@mac.com>
2019-09-07 17:55:39 -0400

    added monkey data model and some Base View Model code

--- Start/MonkeyFinder/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Start/MonkeyFinder/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -5,8 +5,40 @@ using Xamarin.Forms;
 
 namespace MonkeyFinder.ViewModel
 {
-    public class BaseViewModel 
+    public class BaseViewModel : INotifyPropertyChanged
     {
-       
+        bool isBusy;
+        string title;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value)
+                    return;
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (isBusy == value)
+
+                    return;
+
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        
     }
 }
